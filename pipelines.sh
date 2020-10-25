e924979512e675bcb3f9987e53290991aca5186b
Roman Syrtsev <rvs@btower.net>
2020-10-25 15:46:01 +0000

    added required ssm parameters

--- pipelines.sh
+++ pipelines.sh
@@ -10,4 +10,15 @@ aws sts get-caller-identity --profile terraform-infra
 
 # Pipelines
 terraform apply -parallelism=50 -auto-approve -target module.codebuild
+
 terraform apply -parallelism=50 -auto-approve -target module.codedeploy
+
+# Required parameters
+terraform apply -parallelism=50 -auto-approve \
+--target aws_ssm_parameter.database_host \
+--target aws_ssm_parameter.database_port \
+--target aws_ssm_parameter.database_name \
+--target aws_ssm_parameter.database_secret_name \
+--target aws_ssm_parameter.database_secret_arn \
+--target random_password.django_secret \
+--target aws_ssm_parameter.django_secret
